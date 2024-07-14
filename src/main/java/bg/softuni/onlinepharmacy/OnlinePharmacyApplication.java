c1ed26a9eb716a899ea169b39d006c4d176bea6f
5kov <pavlin_petkov@hotmail.com>
2024-07-14 03:44:28 +0300

    Initial commit

---
+++ src/main/java/bg/softuni/onlinepharmacy/OnlinePharmacyApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.onlinepharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePharmacyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePharmacyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bg/softuni/onlinepharmacy/OnlinePharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.onlinepharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePharmacyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
