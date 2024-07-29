ba5dad58fb2e314c6e4896466216c37d2da9cd80
5kov <pavlin_petkov@hotmail.com>
2024-07-29 13:26:04 +0300

    internationalization updated

--- src/main/java/bg/softuni/onlinepharmacy/controller/MedicineController.java
+++ src/main/java/bg/softuni/onlinepharmacy/controller/MedicineController.java
@@ -2,6 +2,7 @@ package bg.softuni.onlinepharmacy.controller;
 
 import bg.softuni.onlinepharmacy.model.entity.Medicine;
 import bg.softuni.onlinepharmacy.repository.MedicineRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,13 +17,14 @@ public class MedicineController {
     private MedicineRepository medicineRepository;
 
     @GetMapping("/item")
-    public String showMedicineDetails(@RequestParam Long medicineId, Model model) {
+    public String showMedicineDetails(@RequestParam Long medicineId, Model model, HttpServletRequest request) {
         Medicine medicine = medicineRepository.findById(medicineId).orElse(null);
         if (medicine == null) {
             model.addAttribute("error", "Medicine not found");
             return "errorPage"; // Redirect to a generic error page or back to list
         }
         model.addAttribute("medicine", medicine);
+        model.addAttribute("request", request);
         return "item";
     }
 }
